c9dd23362941c7a50925b259f525c1e44d1c5b8d
Hongyu Cai <h.tsai@hotmail.com>
2020-09-20 17:54:14 +0800

    Remove constraint of password

--- model/user.go
+++ model/user.go
@@ -7,7 +7,7 @@ type User struct {
 	CreatedAt time.Time
 
 	Email    string `gorm:"type:varchar(100);unique_index;not null"`
-	Password string `gorm:"not null"`
+	Password string
 
 	AccessKey string `gorm:"size:44;unique;not null"`
 	SecretKey string `gorm:"size:44;not null"`
